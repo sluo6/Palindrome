bfc279b94212f1825d66f121113371613a7db6c2
Shangwen Luo <shangwenluo6@gmail.com>
2017-04-21 21:45:33 -0500

    isPalindrome is working. Recursive method to be finished

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -3,12 +3,52 @@ public class Palindrome {
 
 	public static Deque<Character> wordToDeque(String word){
 		int length = word.length();
-		ArrayDeque<String> wDeque = new ArrayDeque<String>();
-		
-		return null;
+		ArrayDeque<Character> wDeque = new ArrayDeque<Character>();
+		for(int i = 0; i < length; i++){
+			wDeque.addLast(word.charAt(i));
+		}	
+		return wDeque;
+	}
+
+/* Using recursion for wordToDeque
+ * public static Deque<Character> wordToDeque(String word){
+ * 		int length = word.length();
+ * 		ArrayDeque<Character> wDeque = new ArrayDeque<Character>();
+ * 		if (length=1){
+ * 			return wDeque.addLast(word.charAt(length-1));
+ * }
+ * 		else
+ * 			return???
+ * 			
+ * word.substring(a,b)
+ * }
+ */
+	private static Deque<Character> wordReverseDeque(String word){
+		int length = word.length();
+		ArrayDeque<Character> rDeque = new ArrayDeque<Character>();
+		for(int i = 0; i < length; i++){
+			rDeque.addFirst(word.charAt(i));
+		}	
+		return rDeque;
 	}
 	
 	public static boolean isPalindrome(String word){
-		return null;
+		if (word.length() == 0 || word.length() == 1){
+			return true;
+		}
+		Deque<Character> wDeque = new ArrayDeque<Character>();
+		Deque<Character> rDeque = new ArrayDeque<Character>();
+		wDeque = wordToDeque(word);
+		rDeque = wordReverseDeque(word);
+		int a = 0;
+			for (int i = 0; i < word.length(); i++){
+				if (wDeque.get(i) == rDeque.get(i)){
+					a++;
+				}
+				if(a == word.length()){
+					return true;
+				}
+			}
+			return false;
 	}
 }
